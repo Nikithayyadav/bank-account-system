d9c50693bab7326f317bead23aa13ab2bc58353c
Nikitha yadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-08-01 18:42:29 +0530

    Update BankController.java

--- src/main/java/com/proficon/bank/controller/BankController.java
+++ src/main/java/com/proficon/bank/controller/BankController.java
@@ -1,7 +1,7 @@
 package com.proficon.bank.controller;
 
 import org.springframework.web.bind.annotation.*;
-import com.proficon.bank.model.BankRequest; 
+import com.proficon.bank.model.BankRequest;  
 import com.proficon.bank.model.BankResponse;
 import com.proficon.bank.service.BankService;
 
