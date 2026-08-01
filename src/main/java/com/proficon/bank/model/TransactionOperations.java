be5269a00cf9c5fc95920e0c7c94fe6446221ad6
Nikitha yadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-08-01 18:43:46 +0530

    Update TransactionOperations.java

--- src/main/java/com/proficon/bank/model/TransactionOperations.java
+++ src/main/java/com/proficon/bank/model/TransactionOperations.java
@@ -3,5 +3,5 @@ package com.proficon.bank.model;
 public interface TransactionOperations {
     boolean deposit(double amount);
     boolean withdraw(double amount);
-    double getBalance();
+    double getBalance(); 
 }
