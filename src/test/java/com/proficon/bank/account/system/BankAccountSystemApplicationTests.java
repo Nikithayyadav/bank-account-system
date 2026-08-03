7b2013fd1604e33a90dc5408751b6cfabe0b4502
Nikitha yadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-08-03 12:06:49 +0530

    Fix formatting in BankAccountSystemApplicationTests

--- src/test/java/com/proficon/bank/account/system/BankAccountSystemApplicationTests.java
+++ src/test/java/com/proficon/bank/account/system/BankAccountSystemApplicationTests.java
@@ -2,7 +2,7 @@ package com.proficon.bank.account.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;  
-
+ 
 @SpringBootTest
 class BankAccountSystemApplicationTests {
 
