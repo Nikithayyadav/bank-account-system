b1c22277474662273f69ea40f539b9624419c8e9
Nikitha yadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-08-03 12:06:23 +0530

    Update BankAccountSystemApplicationTests.java

--- src/test/java/com/proficon/bank/account/system/BankAccountSystemApplicationTests.java
+++ src/test/java/com/proficon/bank/account/system/BankAccountSystemApplicationTests.java
@@ -1,7 +1,7 @@
 package com.proficon.bank.account.system;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest; 
 
 @SpringBootTest
 class BankAccountSystemApplicationTests {
